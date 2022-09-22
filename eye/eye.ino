cc89ac67a2e558caad30a6d2c856969a6db7e7bb
gallahad@me.com <eYgbc6sc6GehM;XafYmUdUC#7mnUNGn>
2022-09-22 13:07:08 -0400

    button changes background color

--- eye/eye.ino
+++ eye/eye.ino
@@ -21,9 +21,25 @@ byte lidDirection = 0;
 unsigned long nextBlink = 1000;
 unsigned long nextBlinkStep = 0;
 
+byte buttonWatcher = HIGH;
+byte currentColor = 0;
+const byte COLOR_COUNT = 7;
+const byte fillColors[COLOR_COUNT][3] = {
+  {0, 0, 255},
+  {255, 0 ,0},
+  {0, 255, 0},
+  {255, 255, 0},
+  {255, 100, 0},
+  {0, 255, 255},
+  {255, 0, 255}
+};
+
 void setup() {
   Serial.begin(9600);
   Serial.println("8x8 LED Matrix Test");
+
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, OUTPUT);
   
   matrix.begin(0x70);
   matrix.setBrightness(0);
@@ -31,6 +47,8 @@ void setup() {
 }
 
 void loop() {
+  listenForButton();
+
   stepPupil();
   stepEyelid();
   
@@ -39,7 +57,26 @@ void loop() {
   drawEyelid();
   matrix.writeDisplay();
 
+  analogWrite(6, fillColors[currentColor][0]);
+  analogWrite(9, fillColors[currentColor][1]);
+  analogWrite(10, fillColors[currentColor][2]);
+
   delay(125);
+}
+
+void listenForButton() {
+  digitalWrite(8, HIGH);
+  byte currentButton = digitalRead(7);
+
+  if (currentButton != buttonWatcher && currentButton == LOW) {
+    currentColor++;
+
+    if (currentColor >= COLOR_COUNT) {
+      currentColor = 0;
+    }
+  }
+
+  buttonWatcher = currentButton;
 }
 
 void stepPupil() {
@@ -104,8 +141,9 @@ void stepEyelid() {
   }
 }
 
+// assumes eye connectors are DOWN
 void drawEyelid() {
   if (lid == 0) return;
   
-  matrix.fillRect(0, 0, 8, lid, LED_OFF);
+  matrix.fillRect(8 - lid, 0, lid, 8, LED_OFF);
 }
